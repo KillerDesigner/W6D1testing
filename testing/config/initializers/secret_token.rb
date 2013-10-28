6a8e301091c7cdd28ede433711bd9f48dc7e5234
KillerDesigner <git@killerdesigner.com>
2013-10-28 09:19:41 -0700

    Initial commit

---
+++ testing/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Testing::Application.config.secret_token = '26ba398c9625df8ee1944588e149e67e98e455259c222131b385c3401cf5cde0fe8d4434c9509997a72ab2019346cb630373d8bc89cb1d39a661a13830aa4416'
